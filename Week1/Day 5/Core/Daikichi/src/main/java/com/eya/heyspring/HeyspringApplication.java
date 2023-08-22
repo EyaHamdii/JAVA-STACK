fbc18c69b67dfecfd456c94bc6e0bc69faa726af
Eya Hamdi <eyahamdi184@gmail.com>
2023-08-22 14:39:00 +0100

    Java

---
+++ Week1/Day 5/Core/Daikichi/src/main/java/com/eya/heyspring/HeyspringApplication.java
@@ -0,0 +1,13 @@
+package com.eya.heyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyspringApplication.class, args);
+	}
+
+}
